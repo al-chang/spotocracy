d5f2532868db08dbe8e52f31fd7a7608aea9aac0
al-chang <aac6692@gmail.com>
2022-04-06 13:54:58 -0400

    Changed socket to depend on port

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import Queue from "./pages/Queue";
-import Header from "./components/Header";
-import { io, Socket } from "socket.io-client";
-import { Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import Queue from './pages/Queue';
+import Header from './components/Header';
+import { io, Socket } from 'socket.io-client';
+import { Box } from '@chakra-ui/react';
 
 const App: React.FC = () => {
-  const [roomID, setRoomID] = useState("");
+  const [roomID, setRoomID] = useState('');
   const [createRoom, setCreateRoom] = useState<boolean>(false);
   const [socket, setSocket] = useState<Socket>();
 
   useEffect(() => {
-    const newSocket = io("http://127.0.0.1:80/");
+    const newSocket = io(`${process.env.REACT_APP_URL}/`);
     setSocket(newSocket);
     return () => {
       newSocket.close();
