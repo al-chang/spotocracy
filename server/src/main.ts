18efcdea2000fc37e626467e19f4a04e02265bda
al-chang <aac6692@gmail.com>
2022-04-06 13:36:11 -0400

    Fixed port using wrong spelling

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: process.env.CLIENT_URL,
+      origin: '*',
       methods: ['GET', 'PUT', 'POST', 'DELETE'],
       allowedHeaders: '*',
       exposedHeaders: '*',
     },
   });
   app.enableCors();
-  await app.listen(process.env.port || 8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
